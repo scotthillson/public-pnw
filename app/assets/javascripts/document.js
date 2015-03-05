f98dbcc618933b20f857def9b70f1c2654a1c977
scotthillson <scott.hillson@gmail.com>
2015-03-04 16:18:42 -0800

    faqs and announcements and stuff

--- app/assets/javascripts/document.js
+++ app/assets/javascripts/document.js
@@ -1,12 +1,3 @@
-var load_twitter = function(d,s,id){
-  var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
-  if(!d.getElementById(id)){
-    js=d.createElement(s);
-    js.id=id;
-    js.src=p+"://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js,fjs);
-  }
-}
 var ready = function(){
   $('.container').popover();
   $('.popover-dismiss').popover({trigger: 'focus'});
@@ -14,9 +5,7 @@ var ready = function(){
   $('.container').on('click','.houser',function(){$(this).popover('toggle');});
   $(document).on('click','.paypal-button',function(){$('#paypal-form').submit();});
   $(document).on('click','.logo',function(){window.location.href='/';});
-  $(document).on('click','.close',function(){$(this).parent().hide();});  
-  load_twitter(document,"script","twitter-wjs");
-  //console.log('all clear boss?');
+  $(document).on('click','.close',function(){$(this).parent().hide();});
 };
 $(document).on('page:load',ready);
 $(document).ready(ready);
