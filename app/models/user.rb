096fe7b7e8902b238ad3ffb5b882e71831716219
scotthillson <scott.hillson@gmail.com>
2015-06-17 22:36:02 -0700

    fuck you git

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :password, on: :create
   before_save :downcase_email
-  
+
   def downcase_email
-    email = email.downcase
+    email = self.email.downcase
   end
-  
+
 end
