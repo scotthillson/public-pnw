9fdc6244a6ada0123b917d9295aca9c5e35f2c27
scotthillson <scott.hillson@gmail.com>
2016-03-12 21:18:23 -0800

    migrations -_-

--- db/migrate/20160310061058_fix_sponsor_url.rb
+++ db/migrate/20160310061058_fix_sponsor_url.rb
@@ -1,5 +1,6 @@
 class FixSponsorUrl < ActiveRecord::Migration
   def change
-    rename_column :sponsors, :sponsor_link, :sponsor_url
+    rename_column :sponsors, :sponsor_link, :sponsor_url if column_exists? :sponsors, :sponsor_link
+    rename_column :sponsors, :link, :sponsor_url if column_exists? :sponsors, :link
   end
 end
