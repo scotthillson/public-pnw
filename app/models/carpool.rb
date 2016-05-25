fad26576c6862daee6e7e4831987104ba2e500ad
scotthillson <scott.hillson@gmail.com>
2016-05-25 15:55:52 -0700

    readable methods

--- app/models/carpool.rb
+++ app/models/carpool.rb
@@ -8,19 +8,31 @@ class Carpool < ActiveRecord::Base
   validates_presence_of :return
   
   def reserve(user)
-    if seats.count < seats_offered
+    if seats_are_still_available
       Seat.reserve(user, id)
     else
       false
     end
   end
   
+  def number_of_seats_taken
+    seats.count
+  end
+  
+  def seats_are_still_available
+    number_of_seats_taken < seats_offered
+  end
+  
   def seats_remaining
-    seats_offered - self.seats.count if seats_offered
+    seats_offered - seats.count if seats_offered
+  end
+  
+  def some_seats_are_taken
+    number_of_seats_taken > 0
   end
   
   def destroy_message
-    if self.seats.count > 0
+    if some_seats_are_taken
       'Do you really want to cancel??'
     else
       'Are you sure you want to cancel this carpool?'
