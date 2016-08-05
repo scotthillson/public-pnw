fec9cdef03989e87fdcb0d4f8cc420e437351721
scotthillson <scott.hillson@gmail.com>
2016-08-05 10:40:25 -0700

    default boots

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,11 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+module Rails
+  class Server
+    def default_options
+      super.merge(Host:  '0.0.0.0', Port: 80)
+    end
+  end
+end
