49c1d6da5b8cc164f02c8b9b5d585d9056b28eeb
scotthillson <scott.hillson@gmail.com>
2016-08-05 10:42:38 -0700

    default boots

--- config/boot.rb
+++ config/boot.rb
@@ -1,12 +1,9 @@
-# Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 
-module Rails
-  class Server
-    def default_options
-      super.merge(Host:  '0.0.0.0', Port: 80)
-    end
+module DefaultOptions
+  def default_options
+    super.merge(Host: '0.0.0.0', Port: 80)
   end
 end
