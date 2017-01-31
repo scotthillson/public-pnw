fbaa0d5a88461a74f975e900ddca0f7505726d99
scotthillson <scott.hillson@gmail.com>
2017-01-30 20:30:45 -0800

    comment test stuff

--- app/models/twilio_message.rb
+++ app/models/twilio_message.rb
@@ -1,14 +1,14 @@
 class TwilioMessage < ActiveRecord::Base
   
   Twilio.configure do |config|
-    config.account_sid = account_sid
-    config.auth_token = auth_token
+    #config.account_sid = account_sid
+    #config.auth_token = auth_token
   end
   
-  @client = Twilio::REST::Client.new
+  #@client = Twilio::REST::Client.new
   
   def self.send(to, from, body)
-    @client.messages.create( from: from, to: to, body: body)
+    #@client.messages.create( from: from, to: to, body: body)
   end
   
 end
